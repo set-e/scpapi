de0033bad782d59dd0a39fe65980297ded702f3b
set-e <ggaeuk@naver.com>
2025-02-03 13:39:26 +0900

    2025/02/03

--- resource/virtual_server.py
+++ resource/virtual_server.py
@@ -4,7 +4,7 @@ from . import Resource
 class VirtualServer(Resource):
     def list(self, query: dict = None):
         self.client.set_method("GET")
-        self.client.set_path('/server-group/v2/server-groups')
+        self.client.set_path('/virtual-server/v2/virtual-servers')
         return self.client.call_api(query=query)
 
     def detail(self, virtual_server_id: str):
